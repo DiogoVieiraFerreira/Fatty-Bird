34701e25492497aa470f50857b17c69ec3355522
Diogo <diogof-6@hotmail.com>
2020-10-01 00:01:08 +0200

    create character

--- main.rb
+++ main.rb
@@ -4,13 +4,25 @@ require 'pp'
 class Game < Gosu::Window 
     def initialize
         super 1280, 720, {resizable: false}
-        self.caption = "Flatty Bird, love fat birds ♥"
+        self.caption = "Fatty Bird, love fat birds ♥"
+
+        Character.new 50, self.height/2-25
     end
     def update
         
     end
     def draw
         
+    end
+end
+
+class Character
+    def initialize x, y, width: 50, height: 50
+        @rectangle = Rectangle2D.new x, y, width, height
+        pp @rectangle
+    end
+    def height
+        @rectangle.width
     end
 end
 
